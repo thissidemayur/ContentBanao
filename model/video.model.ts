fd44b152a3f5bb74afacccfb3e02ec65d62969bc
Mayur Pal <thissidemayur@gmail.com>
2025-06-23 11:09:43 +0530

    chore(video.model.ts): update video schema as per imagekit requriement

--- model/video.model.ts
+++ model/video.model.ts
@@ -1,64 +1,64 @@
 import mongoose, { model, models, Schema } from "mongoose";
 
 export const VIDEO_DIMENSION = {
-    width:1000,
-    height:1920,
+    width: 1000,
+    height: 1920,
 
 } as const
 
 export interface IVideo {
-    _id?:mongoose.Types.ObjectId;
-    title:string;
-    descrition:string;
-    videourl:string;
-    thumbnailUrl:string;
-    controls:boolean;
-    transformation:{
-        height:number;
-        width:number;
-        quantity:number;
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    descrition: string;
+    videourl: string;
+    thumbnailUrl: string;
+    controls: boolean;
+    transformation: {
+        height: number;
+        width: number;
+        quality: number;
     };
 }
 
-export const videoSchema= new Schema({
-    title:{
-        type:String,
-        required:true
-    } ,
-    descrition:{
-      type:String,
-        required:true
-    } ,
-    videourl:{
-        type:String,
-         required:true
+export const videoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
     },
-    thumbnailUrl:{
-        type:String,
-         required:true
+    descrition: {
+        type: String,
+        required: true
     },
-    controls:{
-        type:Boolean,
-        default:true
+    videourl: {
+        type: String,
+        required: true
     },
-    transformation:{
-        height:{
-            type:Number,
-            default:VIDEO_DIMENSION.height
+    thumbnailUrl: {
+        type: String,
+        required: true
+    },
+    controls: {
+        type: Boolean,
+        default: true
+    },
+    transformation: {
+        height: {
+            type: Number,
+            default: VIDEO_DIMENSION.height
         },
-        width:{
-            type:Number,
-            default:VIDEO_DIMENSION.width
+        width: {
+            type: Number,
+            default: VIDEO_DIMENSION.width
         },
-        quantity:{
-            type:Number,
-            min:1,  //from imagekit
-            max:100 // from imagekit
+        quality: {
+            type: Number,
+            min: 1,  //from imagekit
+            max: 100 // from imagekit
         }
     }
 })
 
-const Video = models?.Video || model<IVideo>("Video",videoSchema)
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
 export default Video;
 
 
