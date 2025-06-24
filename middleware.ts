f93987caa69628357af31af5bf61020e4c6960a3
Mayur Pal <thissidemayur@gmail.com>
2025-06-24 11:36:00 +0530

    feat(middleware): add middleware layout for secure page

---
+++ middleware.ts
@@ -0,0 +1,68 @@
+import { withAuth } from "next-auth/middleware"
+import { NextRequest, NextResponse } from "next/server"
+
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+        return NextResponse.next()//pass middleware to either frontend or backend
+
+    },
+    {
+        callbacks: {
+            authorized({ req, token }) {
+                const { pathname } = req.nextUrl // here pathname is API endpoint 
+
+                if (
+                    pathname.startsWith("/api/auth") ||
+                    pathname === '/login' ||
+                    pathname === '/register'
+                ) return true
+
+                if (pathname === "/" || pathname.startsWith("/api/video")) return true
+
+                return !!token // same as if(token) return true ; means if there is token user is authenticated
+            }
+        }
+
+    },
+)
+
+
+export const config = {
+
+    matcher: [
+        /*
+         * Match all request paths except:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - public folder
+         */
+        "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    ],
+};
+
+/******************************* 
+ * 
+1. `req` inside `authorized()` is a `NextRequest` object provided by NextAuth middleware, which safely includes `.nextUrl.pathname`.
+
+2. `req` outside of `authorized()` (in main middleware body) must use `new URL(req.url)` to access pathname reliably.
+
+3. The `authorized()` callback controls access based on authentication logic — it decides **who** gets access.
+
+4. The `config.matcher` decides **when** the middleware even runs — to skip static files or unprotected routes.
+
+5. We exclude `_next/static`, `_next/image`, `favicon.ico`, and `public/` because they serve static assets that do not require session/token validation. Running auth checks on them is inefficient and risky.
+
+6. Together, `matcher` and `authorized()` ensure secure + performant access control.
+
+7.matcher = performance optimization: Don’t even run middleware for static/image files.
+
+8.authorized() = logic control: Who is allowed to proceed for protected routes.
+
+9 `config` is auto-used by Next.js — you don't need to import/use it manually.
+
+10. It controls which routes should trigger middleware. If a route doesn't match, middleware won’t run.
+
+
+ */
