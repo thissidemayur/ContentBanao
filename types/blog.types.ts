90d4d03f09b011cb9223e3327bea9818e3f57fd8
Mayur Pal <thissidemayur@gmail.com>
2025-06-28 00:08:57 +0530

    implement blog interface

---
+++ types/blog.types.ts
@@ -0,0 +1,16 @@
+import { MediaItem } from "./media.types";
+
+export interface Blog {
+
+    title: string,
+    image?: string,
+    isPublished: Boolean,
+    content: string,
+    summary: string,
+    authorId: string,
+    slug: string,
+    media?: MediaItem[], // for image only
+    tags?: string[],
+    createdAt: Date,
+    updatedAt: Date,
+}
