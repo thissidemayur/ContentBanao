d4f55218ded85aab706235a2911a2000117363cd
Mayur Pal <thissidemayur@gmail.com>
2025-07-04 23:42:19 +0530

    fixed auth issue

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -1,10 +1,21 @@
 import { getUploadAuthParams } from "@imagekit/next/server"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth.lib" // adjust path as per your setup
 
 export async function GET() {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        return new Response(
+            JSON.stringify({ error: "Unauthorized" }),
+            { status: 401 }
+        )
+    }
+
     try {
         const { token, expire, signature } = getUploadAuthParams({
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-            publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+            publicKey: process.env.NEXT_AUTH_IMAGEKIT_PUBLIC_KEY as string,
         })
 
         return Response.json(
@@ -12,13 +23,13 @@ export async function GET() {
                 token,
                 expire: Number(expire),
                 signature,
-                publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+                publicKey: process.env.NEXT_AUTH_IMAGEKIT_PUBLIC_KEY,
             },
             { status: 200 }
         )
 
     } catch (error) {
-        console.error("Imagekit authentication failed ::imagekit-aut= ", error)
+        console.error("Imagekit authentication failed ::imagekit-auth= ", error)
         return Response.json(
             { error: "video uploading Authentication failed" },
             { status: 500 }
