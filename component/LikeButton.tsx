1cccfeb0de91a96df8b110dfb0d6272dde33e217
Mayur Pal <thissidemayur@gmail.com>
2025-07-05 23:46:57 +0530

    implement like button

--- component/LikeButton.tsx
+++ component/LikeButton.tsx
@@ -1,38 +1,64 @@
-"use client"
+"use client";
 
+import { useLikeBlogMutation } from "@/features/blogs/blogsApi";
+import { useAuth } from "@/hooks/userAuth";
+import React, { useState } from "react";
+import { Heart } from "lucide-react";
 
+type Props = {
+  slug: string;
+  likes: number;
+  liked: boolean;
+};
 
-import { useLikeBlogMutation } from '@/features/blogs/blogsApi';
-import { useAuth } from '@/hooks/userAuth'
-import React, { useState } from 'react'
+export default function LikeButton({ slug, likes, liked }: Props) {
+  const { isAuthenticated } = useAuth();
 
-export default function LikeButton({ slug, initialLikes, initialLiked }) {
-    const { userAuth, isAuthenticated } = useAuth()
+  const [likeBlog, { isLoading }] = useLikeBlogMutation();
 
-    const [likes, setLikes] = useState(initialLikes);
-    const [liked, setLiked] = useState(initialLiked);
+  const [isLiked, setIsLiked] = useState(liked);
+  const [likeCount, setLikeCount] = useState(likes);
 
-    const [likeResponse] = useLikeBlogMutation(slug)
-    const toggleLike = async () => {
-        if (!isAuthenticated) {
-            alert("Please login first!");
-            return;
-        }
-        setLiked(likeResponse.liked)
-        setLikes(likeResponse.likes)
-
-
-
+  const toggleLike = async () => {
+    if (!isAuthenticated) {
+      alert("Please login first!");
+      return;
     }
 
-    return (
-        <button
-            onClick={toggleLike}
-            className={`text-xl px-4 py-2 rounded-lg flex items-center gap-2 ${liked ? "text-red-500" : "text-gray-500"
-                }`}
-        >
-            ❤️ {likes}
-        </button>
+    try {
+      await likeBlog(slug).unwrap();
+      setIsLiked((prev) => !prev);
+      setLikeCount((prev) => (isLiked ? prev - 1 : prev + 1));
+    } catch (err) {
+      console.error("Failed to toggle like: ", err);
+    }
+  };
 
-    )
+  return (
+    <button
+      onClick={toggleLike}
+      disabled={isLoading}
+      className="flex items-center gap-1 px-3 py-2 rounded-full transition duration-200"
+    >
+      <div
+        className={`p-1 rounded-full transition-colors duration-200 ${
+          isLiked ? "bg-red-100 text-red-500" : "bg-gray-100 text-gray-500"
+        }`}
+      >
+        <Heart
+          size={20}
+          fill={isLiked ? "#f43f5e" : "none"} // this fills actual icon background
+          className="transition duration-200"
+        />
+      </div>
+
+      <span
+        className={`text-sm font-medium transition-colors duration-200 ${
+          isLiked ? "text-red-500" : "text-gray-600"
+        }`}
+      >
+        {likeCount}
+      </span>
+    </button>
+  );
 }
