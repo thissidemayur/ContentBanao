bb4f9e899714ec54d8cebff6133aec6803260d30
Mayur Pal <thissidemayur@gmail.com>
2025-07-12 11:42:15 +0530

    implement search @ server side

---
+++ app/api/search/route.ts
@@ -0,0 +1,33 @@
+import { connectToDB } from "@/lib/db.lib";
+import Blog from "@/model/blog.model";
+import User from "@/model/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const q = searchParams.get("q") || "";
+    const type = searchParams.get("type") || "blog"; // default search
+
+    if (!q) {
+        return NextResponse.json({ data: [], message: "No keyword" });
+    }
+
+    await connectToDB();
+
+    let results = [];
+
+    if (type === "user") {
+        results = await User.find({
+            userName: { $regex: q, $options: "i" },
+        }).select("userName avatar _id");
+    } else if (type === "blog") {
+        results = await Blog.find({
+            $or: [
+                { title: { $regex: q, $options: "i" } },
+                { description: { $regex: q, $options: "i" } },
+            ],
+        }).select("title slug thumbnailUrl");
+    }
+
+    return NextResponse.json({ data: results });
+}
