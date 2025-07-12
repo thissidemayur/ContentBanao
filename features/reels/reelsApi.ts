e8559ae6272cd05b0398ba386c9b2ea66858fe35
Mayur Pal <thissidemayur@gmail.com>
2025-07-12 11:59:06 +0530

    implement reel rtk query

---
+++ features/reels/reelsApi.ts
@@ -0,0 +1,65 @@
+import { IVideo } from '@/model/reels.model';
+import { MediaItem } from '@/types/media.types';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import mongoose from 'mongoose';
+
+export const reelApi = createApi({
+    reducerPath: "reelApi",
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_AUTH_BACKEND_BASE_URL, credentials: "include" }),
+    tagTypes: ["reel"],
+    endpoints: (build) => ({
+        // get Reel by Id
+        getReelById: build.query<IVideo, string>({
+            query: (id) => `/reel/${id}`,
+
+        }),
+
+        // get reel
+        getReel: build.query<{ message: string, data: IVideo[], hasMore: boolean, totalVideo: SVGAnimatedNumberList }, { page: number, limit: number }>({
+            query: ({ page = 1, limit = 5 }) => ({
+                url: `/reel?page=${page}&limit=${limit}`,
+                method: "GET",
+            }),
+            providesTags: (result) =>
+                result?.data
+                    ? result.data.map(({ _id }) => ({ type: "reel" as const, id: _id.toString() }))
+                    : ["reel"],
+        }),
+
+        // create Reel
+        createReel: build.mutation<void, { title: string, description: string, tags: string[], videoUrl: string }>({
+            query: ({ title, description, tags, videoUrl }) => ({
+                url: "/reel",
+                method: "POST",
+                body: { title, description, tags, videoUrl },
+
+            }),
+            invalidatesTags: ["reel"],
+
+        }),
+
+        // delete Reel
+        deleteReel: build.mutation<void, string>({
+            query: (id) => ({
+                url: `/reel/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["reel"],
+
+        }),
+
+        // like reel
+        likeReel: build.mutation<{ liked: boolean; likesCount: number; message: string }, string>({
+            query: (id) => ({
+                url: `/reel/${id}/like`,
+                method: "POST",
+            }),
+            invalidatesTags: ["reel"],
+
+        }),
+    })
+})
+
+
+export const { useCreateReelMutation, useDeleteReelMutation, useGetReelByIdQuery, useGetReelQuery, useLikeReelMutation } = reelApi
+
